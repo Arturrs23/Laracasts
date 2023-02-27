f04292bffaf1eeff886866b8674178ee2415092a
Arturs <arrturs.torsters@gmail.com>
2023-02-27 14:19:29 +0200

    test

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
 // handling the requests
-$router->get('/', 'controllers/index.php');
+$router->get('/', 'index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
@@ -9,4 +9,11 @@ $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 
 $router->get('/notes/create', 'controllers/notes/create.php');
+$router->post('/notes', 'controllers/notes/store.php');
+
+
+
+
+$router->get('/register/create', 'controllers/notes/create.php');
+
 $router->post('/notes', 'controllers/notes/store.php');
